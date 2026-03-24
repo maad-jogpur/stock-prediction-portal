79ec32c665e56072c9a472cb82335c19adf0be84
maad-jogpur <maadjogpur@gmail.com>
2026-03-24 16:11:14 +0530

    Registration Page

--- backend-drf/accounts/serializers.py
+++ backend-drf/accounts/serializers.py
@@ -2,10 +2,11 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only = True, style = {'input_type':'password'})
+    password = serializers.CharField(write_only=True,min_length=8, style = {'input_type':'password'})
     class Meta:
         model = User
         fields = ['username','email','password']
+        
     
     def create(self, validated_data):
         # User.objects.create = save the password in a plain text
